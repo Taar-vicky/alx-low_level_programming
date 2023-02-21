e12572c2d1387328b817ae1fe88acb2f50ac254f
Taar-Vicky <vous52878@gmail.com>
2023-02-21 10:12:42 -0800

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print alphabet - make the alphabet
- * Return: Always 0
+ * Return: void
  */
 
 void print_alphabet(void)
@@ -12,7 +12,7 @@ void print_alphabet(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
