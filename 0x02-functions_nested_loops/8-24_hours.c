75aca7e2ff82b57249d69706a58e3ce8546f5e9e
Taar-Vicky <vous52878@gmail.com>
2023-02-22 01:01:40 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,6 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	{
 	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
@@ -21,6 +20,7 @@ void jack_bauer(void)
 	for (c = 0; c <= 5; c++)
 	{
 	for (d = 0; d <= 9; d++)
+	{
 	_putchar(a + '0');
 	_putchar(b + '0');
 	_putchar(58);
