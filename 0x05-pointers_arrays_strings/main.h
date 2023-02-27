bfcb9653f2f1a652c6384f769a986ead19f2259f
Taar-Vicky <vous52878@gmail.com>
2023-02-27 13:33:27 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,5 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
+int _atoi(char *s);
 
 #endif
