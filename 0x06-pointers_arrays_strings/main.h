3c14c1d5f36229cb4d4510d72a152e7d2c8711c7
Taar-Vicky <vous52878@gmail.com>
2023-03-02 13:48:24 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -11,6 +11,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-char *_strstr(char *haystack, char *needle);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 #endif
